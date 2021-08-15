b52f2a839a950e3f9d97da389f9dcb46670892a5
Manuel Villarreal <manolovillarreal@hotmail.com>
2021-08-15 16:42:36 -0500

    se implemento el uid y la busqueda por objeto

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -7,32 +7,44 @@ const usuariosPost = async(req, res) => {
     const { username, password } = req.body
 
     //#region Validaciones
+    //Verifica que el request contenga el usuario
     if (!username) {
-        res.status(400).json({
+        return res.status(400).json({
             msg: "Debe enviar el usuario"
         })
     }
+    //Verifica que el request contenga la contraseña
     if (!password) {
-        res.status(400).json({
+        return res.status(400).json({
             msg: "Debe enviar el password"
+        })
+    }
+    //Verifica si ya existe usuario con ese username
+    const usuarioDB = await Usuario.findOne({ username })
+    if (usuarioDB) {
+        return res.status(400).json({
+            msg: "Ya existe usuario con ese username"
         })
     }
     //#endregion
 
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
-    password = bcryptjs.hashSync(password, salt);
+    const hash = bcryptjs.hashSync(password, salt);
 
-    await Usuario.save({
-
+    //preparo la data para guardar
+    const dataUsuario = {
         username,
-        password
-    })
+        password: hash,
+        estado: true
+    }
 
+    //Guardamos el usuario y extraemos la contraseña 
+    const { password: pass, ...usuario } = await Usuario.save(dataUsuario)
 
     res.json({
         msg: 'POST - api',
-        usuario: req.body
+        usuario
     })
 }
 
--- models/usuario.js
+++ models/usuario.js
@@ -3,6 +3,7 @@ const { model, Schema } = require('../jsonDB/jsonDB');
 const UsuarioSchema = Schema({
     id: Number,
     username: String,
+    estado: Boolean,
     score: Number,
     achievements: Array
 })
